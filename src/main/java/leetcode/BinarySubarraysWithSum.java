30e2ac72ea72a46b936fc175742080d459a9132c
kunal-das <kd.kunaldas92@gmail.com>
2022-03-26 16:55:36 +0530

    Leetcode > 930 > BinarySubarraysWithSum > Optimized

--- src/main/java/leetcode/BinarySubarraysWithSum.java
+++ src/main/java/leetcode/BinarySubarraysWithSum.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.HashMap;
+
 public class BinarySubarraysWithSum {
 
     public static void main(String[] args) {
@@ -8,8 +10,31 @@ public class BinarySubarraysWithSum {
         int[] nums = {0,0,0,0,0};
         int goal = 0;
 
-        int res = subarraysCountWithSumGoalBrute(nums, goal);
+//        int res = subarraysCountWithSumGoalBrute(nums, goal);
+        int res = subarraysCountWithSumGoalOptimized(nums, goal);
         System.out.println(res);
+    }
+
+    private static int subarraysCountWithSumGoalOptimized(int[] nums, int goal) {
+        int i = 0; // left pointer
+        int j = 0; // right pointer
+        int len = nums.length;
+        int sum=0, res=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while(j < len) {
+
+            sum = sum + nums[j];
+            if(sum == goal) res++;
+            if(map.containsKey(sum-goal)) {
+                res += map.get(sum-goal);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+
+            j++;
+        }
+
+        return res;
     }
 
     private static int subarraysCountWithSumGoalBrute(int[] nums, int goal) {
