57d84d7adbc0cef0a6d63f48d0526d073bde82c6
kunal.das <kunal.das@daxko.com>
2022-04-09 09:55:38 +0530

    coding blocks > patterns > Pattern18

---
+++ src/main/java/codingblocks/lec3/patterns/Pattern18.java
@@ -0,0 +1,70 @@
+package codingblocks.lec3.patterns;
+
+import java.util.Scanner;
+
+/*
+
+            *
+        *	* 	*
+    *	*	*	*	*
+ *	*	*	*	*	*	*
+    *	*	*	*	*
+        *	*	*
+            *
+ */
+public class Pattern18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printPattern(n);
+    }
+
+    private static void printPattern(int n) {
+
+        int rows = n;
+        int row = 1;
+        int spaces = n/2;
+        int stars = 0;
+
+        while (row <= rows) {
+
+            int space1 = 1;
+            int star1 = 1;
+            while (space1 <= spaces) {
+                System.out.print("\t");
+                space1++;
+            }
+
+            while (star1 <= stars) {
+                System.out.print("*\t");
+                star1++;
+            }
+
+            System.out.print("*\t"); // print mirror line
+
+            int star2 = 1;
+            int space2 = 1;
+            while (star2 <= stars) {
+                System.out.print("*\t");
+                star2++;
+            }
+
+            while (space2 <= spaces) {
+                System.out.print("\t");
+                space2++;
+            }
+
+            System.out.println();
+            row++;
+            if(row <= (n/2+1)) {
+                spaces -= 1;
+                stars += 1;
+            } else {
+                spaces += 1;
+                stars -= 1;
+            }
+        }
+    }
+}
