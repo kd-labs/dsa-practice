71e4b6003ef2890472c8f8fc675909330bae24ae
kunal.das <kunal.das@daxko.com>
2022-04-09 09:21:56 +0530

    coding blocks > patterns > Pattern15

---
+++ src/main/java/codingblocks/lec3/patterns/Pattern15.java
@@ -0,0 +1,44 @@
+package codingblocks.lec3.patterns;
+
+import java.util.Scanner;
+
+public class Pattern15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printPattern(n);
+    }
+
+    private static void printPattern(int n) {
+        int rows = 2*n;
+        int row = 1;
+        int spaces = 0;
+        int stars = n;
+
+        while (row < rows) {
+
+            int space = 1;
+            int star = 1;
+            while (space <= spaces) {
+                System.out.print("  ");
+                space++;
+            }
+
+            while (star <= stars) {
+                System.out.print("* ");
+                star++;
+            }
+
+            System.out.println();
+            row++;
+            if(row <= n) {
+                spaces += 2;
+                stars -= 1;
+            } else {
+                spaces -= 2;
+                stars += 1;
+            }
+        }
+    }
+}
