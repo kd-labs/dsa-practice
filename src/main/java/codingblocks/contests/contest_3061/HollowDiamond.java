6fc86c5c48c059daf6038473c2ab9b90f9c769f8
kunal.das <kunal.das@daxko.com>
2022-04-10 10:51:01 +0530

    Coding Blocks > Contests > 3061 > HollowDiamond

---
+++ src/main/java/codingblocks/contests/contest_3061/HollowDiamond.java
@@ -0,0 +1,66 @@
+package codingblocks.contests.contest_3061;
+
+import java.util.Scanner;
+
+/*
+Pattern 19:
+ *	*	*	*	*
+ *	*		*	*
+ *				*
+ *	*		*	*
+ *	*	*	*	*
+ */
+public class HollowDiamond {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printPattern(n);
+    }
+
+    private static void printPattern(int n) {
+        int rows = n;
+        int row = 1;
+        int spaces = 1;
+        int stars = n/2;
+
+        while(row <= rows) {
+
+            int star = 1;
+            while (star <= stars) {
+                System.out.print("*\t");
+                star++;
+            }
+
+            int space = 1;
+            while (space <= spaces) {
+
+                if(space == 1 || space == spaces) {
+                    System.out.print("*\t");
+                } else{
+                    System.out.print("\t");
+                }
+
+                space++;
+            }
+
+            star = 1;
+            while (star <= stars) {
+                System.out.print("*\t");
+                star++;
+            }
+
+            if(row <= n/2) {
+                stars -= 1;
+                spaces += 2;
+            } else {
+                stars += 1;
+                spaces -= 2;
+            }
+
+            System.out.println();
+            row++;
+        }
+    }
+}
