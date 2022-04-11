1639591596c2c529b094c57d0453acf7310591c3
kunal.das <kunal.das@daxko.com>
2022-04-11 06:24:35 +0530

    Coding Blocks > Lec4 > Pascal Triangle Explanation added in comments

--- src/main/java/codingblocks/contests/contest_3061/PascalTriangle.java
+++ src/main/java/codingblocks/contests/contest_3061/PascalTriangle.java
@@ -11,6 +11,19 @@ public class PascalTriangle {
         printPascalTriangle(n);
     }
 
+    /*
+        Observation:
+            - Every row of pascal triangle starts with 1 i.e. nC0
+        Concept Used:
+            - nCr
+            - nCr+1 = (nCr * (n-r))/r+1
+                where:
+                    n = row#
+                    r = col#
+                meaning if we have calculated value for nth row and rth col, then to calculate for next col in same row,
+                we can use the formula and not calculate nCr+1 from scratch.
+     */
+
     private static void printPascalTriangle(int n) {
         int rows = n;
         int row = 0;
@@ -22,7 +35,7 @@ public class PascalTriangle {
             int val = 1;
             while (i < nos) {
                 System.out.print(val+"\t");
-                val = (val*(row-i)/(i+1));
+                val = (val*(row-i)/(i+1)); // nCr+1 = ((nCr * (n-r))/r+1)
                 i++;
             }
 
