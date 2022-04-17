9ceca8c4622508fd6e0c484e69f89d7ce0f10710
kunal.das <kunal.das@daxko.com>
2022-04-17 11:44:20 +0530

    Coding Blocks > Contest > InverseNumber

---
+++ src/main/java/codingblocks/contests/contest_3061/InverseNumber.java
@@ -0,0 +1,24 @@
+package codingblocks.contests.contest_3061;
+
+import java.util.Scanner;
+
+public class InverseNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int res = inverseNumber(n);
+        System.out.println(res);
+    }
+
+    private static int inverseNumber(int n) {
+        int res = 0;
+
+        for(int i = 1; n > 0; n = n/10, i++) {
+            res += i * Math.pow(10, n%10);
+        }
+
+        return res/10;
+    }
+}
