b8ca33d8ae1c24a1e4d45d7a79a3d020042d3251
kd <kd.kunaldas92@gmail.com>
2022-05-08 12:33:08 +0530

    Coding Blocks > LeetCode > Binary Search > First Bad Version

--- src/main/java/leetcode/FirstBadVersion.java
+++ src/main/java/leetcode/FirstBadVersion.java
@@ -10,15 +10,15 @@ public class FirstBadVersion {
         int l = 1, h = n;
 
         int res = -1, mid;
-        while(l < h) {
+        while(l <= h) {
             mid = l + (h-l)/2;
             if(isBadVersion(mid)) {
                 res = mid;
-                h = mid;
+                h = mid-1;
             } else {
                 l = mid+1;
             }
         }
-        return l;
+        return res;
     }
 }
