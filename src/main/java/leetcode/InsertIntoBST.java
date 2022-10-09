999f66336c8b40f9c573e3346142c03286b16cce
kd <kd.kunaldas92@gmail.com>
2022-10-09 14:02:52 +0530

    Coding Blocks > Leetcode > Trees > InsertIntoBST

--- src/main/java/leetcode/InsertIntoBST.java
+++ src/main/java/leetcode/InsertIntoBST.java
@@ -4,12 +4,19 @@ import codingblocks.tree.TreeNode;
 
 public class InsertIntoBST {
 
+    /*
+        ThoughtProcess : To add val as leaf node
+     */
     public TreeNode insertIntoBST(TreeNode node, int val) {
 
+        // Base Case : When node is null, then create TreeNode with val and return
         if(node == null) {
             return new TreeNode(val);
         }
 
+        // Recursive Case
+        //  If val is smaller than node's val, then go to left sub-tree
+        //  If val is greater than node's val, then go to right sub-tree
         if(node.data < val) {
             node.right = insertIntoBST(node.right, val);
         } else {
