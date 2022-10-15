589c8eac2aedaa3fcf8bd5b8a8651bdd03db9dda
kd <kd.kunaldas92@gmail.com>
2022-10-15 13:57:53 +0530

    Coding Blocks > Leetcode > Trees > CreateTreeUsingLevelOrder

---
+++ src/main/java/codingblocks/tree/CreateTreeUsingLevelOrder.java
@@ -0,0 +1,43 @@
+package codingblocks.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class CreateTreeUsingLevelOrder {
+
+    private TreeNode root;
+
+    public CreateTreeUsingLevelOrder() {
+        this.root = createTree();
+    }
+
+    private TreeNode createTree() {
+
+        Queue<TreeNode> q = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+
+        int item = sc.nextInt();
+        TreeNode root = new TreeNode(item);
+        TreeNode nn = null;
+        q.add(root);
+        while(!q.isEmpty()) {
+            TreeNode n = q.remove();
+            item = sc.nextInt();
+            if(item != -1) {
+                nn = new TreeNode(item);
+                q.add(nn);
+                n.left = nn;
+            }
+
+            item = sc.nextInt();
+            if(item != -1) {
+                nn = new TreeNode(item);
+                q.add(nn);
+                n.right = nn;
+            }
+        }
+
+        return root;
+    }
+}
