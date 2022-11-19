26900a41f257d730fc28a2c588164bf343a8fff6
kunal-das <kd.kunaldas92@gmail.com>
2022-11-19 23:20:01 +0530

    DSA > Coding Blocks > Heap > Min Heap with removal

--- src/main/java/track/heap/MinHeapDriver.java
+++ src/main/java/track/heap/MinHeapDriver.java
@@ -1,11 +1,12 @@
 package track.heap;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MinHeapDriver {
     public static void main(String[] args) {
 
-        int[] arr = {10, 20, 30, 5, 7, 3, 2, -10};
+        int[] arr = {10, 20, 30, 5, 7, 3, 2, -10, -20};
 
         MinHeap minHeapObj = new MinHeap(arr);
         ArrayList<Integer> minHeap = minHeapObj.getMinHeap();
@@ -13,12 +14,32 @@ public class MinHeapDriver {
         for(Integer i : minHeap) {
             System.out.print(i + "\t");
         }
+        System.out.println();
+
+        System.out.println("Min in heap " + minHeapObj.removeMin());
+        System.out.println("After removing minimum");
+
+        for(Integer a : minHeapObj.getMinHeap()) {
+            System.out.print(a + "\t");
+        }
+        System.out.println();
+
+        System.out.println("Min in heap " + minHeapObj.removeMin());
+        System.out.println("After removing minimum");
+
+        for(Integer a : minHeapObj.getMinHeap()) {
+            System.out.print(a + "\t");
+        }
+        System.out.println();
+
+
     }
 }
 
 class MinHeap {
 
-    private ArrayList<Integer> minHeap;
+    private List<Integer> minHeap;
+    private int size;
 
     public MinHeap(int[] arr) {
 
@@ -27,6 +48,7 @@ class MinHeap {
         for(int i : arr) {
             this.add(i);
         }
+        this.size = this.minHeap.size();
 
     }
 
@@ -39,11 +61,70 @@ class MinHeap {
         upHeapify(this.minHeap.size()-1);
     }
 
+    public int getMin() {
+        return this.minHeap.get(0);
+    }
+
+    public int removeMin() {
+
+        int min = this.minHeap.get(0);
+
+        // set 0th index to Integer max
+        this.minHeap.set(0, Integer.MAX_VALUE);
+
+        downHeapify(0);
+
+        this.size--;
+        this.minHeap = this.minHeap.subList(0, this.size);
+
+        return min;
+
+    }
+
+    private void downHeapify(int parentIndex) {
+
+        // get left child index and right child index
+        int leftChildIdx = 2*parentIndex+1;
+        int rightChildIdx = 2*parentIndex+2;
+
+        // check if leftChildIdx and rightChildIdx is with size of heap
+        // if rightChildIdx is within size and right is not then compare parent with left child
+        // if leftChild is smaller than parent, then swap
+        if(rightChildIdx < this.size) {
+
+            // get the min between left child and right child
+            int min = Math.min(this.minHeap.get(leftChildIdx), this.minHeap.get(rightChildIdx));
+
+            if(min < this.minHeap.get(parentIndex) && min == this.minHeap.get(leftChildIdx)) { // if min is left child, then swap between left child and parent
+                swap(leftChildIdx, parentIndex);
+                // again compare parent with it's children
+                downHeapify(leftChildIdx);
+            } else {
+                swap(rightChildIdx, parentIndex);
+                // again compare parent with it's children
+                downHeapify(rightChildIdx);
+            }
+
+
+        } else if(leftChildIdx < this.size) {
+
+            // left child, if with size, will always be smaller than Integer.MAX
+            // swap left child with parent
+            swap(leftChildIdx, parentIndex);
+
+            // recursive call to downHeapify to again check parent with children
+            downHeapify(leftChildIdx);
+
+        } else  // base case, max value has reached it's correct place
+            return;
+
+    }
+
     public ArrayList<Integer> getMinHeap() {
         return new ArrayList<>(this.minHeap);
     }
 
-    public void upHeapify(int childIndex) {
+    private void upHeapify(int childIndex) {
 
         // calculate parent index
         int parentIndex = (childIndex-1)/2;
