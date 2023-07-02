8950e172af936216055fb4fa44f408de16c3a9bb
kunal-das <kd.kunaldas92@gmail.com>
2023-07-02 12:50:29 +0530

    WIP : SudokuSolver

---
+++ src/main/java/practice_july_2023/SudokuSolver.java
@@ -0,0 +1,8 @@
+package practice_july_2023;
+
+public class SudokuSolver {
+
+    public void solveSudoku(char[][] board) {
+
+    }
+}
---
+++ src/test/java/practice_july_2023/SudokuSolverTest.java
@@ -0,0 +1,7 @@
+package practice_july_2023;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SudokuSolverTest {
+
+}
